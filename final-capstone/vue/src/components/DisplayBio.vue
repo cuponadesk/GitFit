558dc6803fa81d2a4d77836d60da54fdd63abe9f
Dom Ceccoli <domenic.ceccoli1@gmail.com>
2021-04-09 11:06:22 -0400

    resolved issue with routing to trainer page, began styling trainer page

--- final-capstone/vue/src/components/DisplayBio.vue
+++ final-capstone/vue/src/components/DisplayBio.vue
@@ -1,5 +1,32 @@
 <template>
-  <bio/>
+ <!-- <bio/> -->
+<div>
+  <div class="container">
+  <div class="row">
+    <div class="text">Jaclyn Spangler</div>
+    <div id="jaclyn-image" class="image">
+      <img src="images\jac-trainer.jpg" alt="Jaclyn Trainer">
+    </div>
+  </div>
+  <div class="row">
+    <div class="image">image</div>
+    <div class="text">John Baringhaus</div>
+  </div>
+  <div class="row">
+    <div class="text">Zack Sempsrott</div>
+    <div class="image">image</div>
+  </div>
+  <div class="row">
+    <div class="image">image</div>
+    <div class="text">Jamal Stephenson</div>
+  </div>
+  <div class="row">
+    <div class="text">Domenic Ceccoli</div>
+    <div class="image">image</div>
+  </div>
+</div>
+</div>
+  
 </template>
 
 <script>
@@ -10,5 +37,13 @@ export default {
 </script>
 
 <style>
-
+div#jaclyn-image {
+  display: block;
+  max-height: auto;
+  max-width: 25px;
+}
 </style>
+
+
+
+
