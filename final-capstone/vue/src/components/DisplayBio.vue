77d8fcc00dbed65fc09a8badb6271c5e8d7c7c6b
Dom Ceccoli <domenic.ceccoli1@gmail.com>
2021-04-12 11:41:43 -0400

    Added trainer bios to displayBio component in Vue

--- final-capstone/vue/src/components/DisplayBio.vue
+++ final-capstone/vue/src/components/DisplayBio.vue
@@ -5,30 +5,53 @@
       <h3>Jaclyn "The Cardio Queen" Spangler</h3>
       <div class="bio">
         <img src="images\jac-trainer.jpg" alt="Jaclyn Trainer" />
-        <div class="about">
-          <p>Jaclyn has been a trainer with us for 10 years.</p>
-        </div>
+        <p>
+          Jaclyn, better known as the Cardio Queen,<br />
+          knows you dont need to only hit the treadmill or <br />
+          Stairmaster to get your cardio in. She has been <br />
+          with the git fit fam for 6 years and loves pushing <br />
+          people to be their best version.
+        </p>
       </div>
       <h3>John "The Chest Crusader" Baringhaus</h3>
       <div class="bio">
-        <img src="images\jac-trainer1.jpg" alt="John Trainer">
-        <div class="about">
-          <p>John has been a professional bodybuilder since he was 12.</p>
-        </div>
+        <img src="images\jac-trainer1.jpg" alt="John Trainer" />
+        <p>
+          John "The Chest Crusader" is a Certified Strength <br />and Condition
+          Specialist (CSCS) with the National<br />
+          Strength and Conditioning Association. He has an <br />extensive
+          background from training professional <br />
+          and amateur athletes to working professionals with <br />nominal
+          experience in fitness. John approaches <br />fitness as a life long
+          behavior modification where we <br />have continuous improvement. He
+          defines his sessions<br />
+          as simple, challenging, and efficient with an emphasis<br />
+          on proper form and mobility. His favorite client is anyone <br />
+          with a goal and a willingness to work towards it!
+        </p>
       </div>
       <h3>Zack "The Arms Assassin" Sempsrott</h3>
       <div class="bio">
-          <img src="images\jac-trainer2.jpg" alt="Zack Trainer">
-          <p>Zack has been working on his biceps since the 4th grade.</p>
+        <img src="images\jac-trainer2.jpg" alt="Zack Trainer" />
+        <p>
+          Zack believes in working out with an intense "no pain, <br />
+          no gain" mindset. Though he stands behind the <br />importance of
+          working the full body, he was given the <br />
+          nickname "The Arms Assassin" when he started working his <br />biceps
+          in the fourth grade. If you need a trainer who will <br />hold you
+          accountable and push you to your limits, <br />
+          Zack would be a great choice.
+        </p>
       </div>
       <h3>Jamal "The Back Behemoth" Stephenson</h3>
       <div class="bio">
-          <img src="images\jumping-girl.jfif" alt="Jamal">
-          <p>Jamal likes to work out a lot.</p>
+        <img src="images\jumping-girl.jfif" alt="Jamal" />
+        <p>Jamal likes to work out a lot.</p>
       </div>
       <h3>Domenic "The Abs Ambassador" Ceccoli</h3>
       <div class="bio">
-        <div class="image">image</div>
+        <img src="images\IMG_1976.jpg" alt="Dom Trainer" />
+        <p>Domenic has been a lifetime advocate of core strength, <br> and his ab workout routines have taken him all over the <br>world, earning him the nickname "The Abs Ambassador".<br> Domenic believes that core strength is the baseline of<br> physical fitness, and full body fitness stems from a solid core.</p>
       </div>
     </div>
   </body>
@@ -42,7 +65,6 @@ export default {
 </script>
 
 <style>
-
 img {
   height: 50%;
   width: 40%;
@@ -58,18 +80,17 @@ body {
   padding: 20px;
   margin-bottom: 20px;
   border: solid 1px #555;
-  display: flex; 
+  display: flex;
 }
 
 .bio img {
   width: 150px;
   border-radius: 999px;
   margin: 0 10px;
- 
 }
 
-.bio:nth-child(2) img {
-  display: flex;
+.bio:nth-child(2n) img {
+  /*display: flex;*/
   order: 2;
 }
 
@@ -78,8 +99,6 @@ h3 {
   justify-content: center;
   display: flex;
 }
-
-
 </style>
 
 
