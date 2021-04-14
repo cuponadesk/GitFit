c51ddacae45abea67d0ee635094cc465b4ed1f10
Jaclyn Spangler <jaclynkaressa@gmail.com>
2021-04-14 14:52:17 -0400

    added color and bold to nickname

--- final-capstone/vue/src/components/DisplayBio.vue
+++ final-capstone/vue/src/components/DisplayBio.vue
@@ -6,7 +6,7 @@
       <div class="bio">
         <img src="images\jac-trainer.jpg" alt="Jaclyn Trainer" />
         <p>
-          Jaclyn, better known as the Cardio Queen,
+          Jaclyn, aka<span style="color: #5e62d1; font-weight: bold"> "Cardio Queen"</span>
           knows there is more to cardio than just hitting the treadmill or Stairmaster. Jaclyn's workouts are fun, intense, and target multiple major muscle groups to strengthen them while working on cardio. She has been 
           with the Git Fit family for 6 years and loves pushing
           people to be their best version. 
@@ -16,7 +16,7 @@
       <div class="bio">
         <img src="images/john-2.jpg" alt="John Trainer" />
         <p>
-          John "The Chest Crusader" is a Certified Strength and Condition
+          John <span style="color: #5e62d1; font-weight: bold">"The Chest Crusader" </span> a Certified Strength and Condition
           Specialist (CSCS) with the National
           Strength and Conditioning Association. He has an extensive
           background from training professional 
@@ -36,7 +36,7 @@
           Zack believes in working out with an intense "no pain,
           no gain" mindset. Though he stands behind the importance of
           working the full body, he was given the
-          nickname "The Arms Assassin" when he started working his biceps
+          nickname <span style="color: #5e62d1; font-weight: bold">"The Arms Assassin"</span> when he started working his biceps
           in the fourth grade. If you need a trainer who will hold you
           accountable and push you to your limits,
           Zack would be a great choice.
@@ -46,12 +46,12 @@
       <div class="bio">
         <img src="\images\GOPR0168[1].JPG" alt="Jamal" />
         <p> "Mamba Mentality, A constant quest to be the best version of yourself". As a trainer that is all I ask, yes it will be hard, no you can not take a break, and yes you will get great results. Come to the gym to see friends, hang out, and take selfies? Sorry not the trainer for you (selfies are for post workout to admire gains).
-          You can not out work a bad diet (I've tried!) when choosing myself as your trainer not only do you get the intensity of a former D1 athlete you also recieve the nutritional guidance to upgrade to you 2.0! Known around these parts as "The Back Behemoth", you'll find out why  </p>
+          You can not out work a bad diet (I've tried!) when choosing myself as your trainer not only do you get the intensity of a former D1 athlete you also recieve the nutritional guidance to upgrade to you 2.0! Known around these parts as <span style="color: #5e62d1; font-weight: bold">"The Back Behemoth"</span>, you'll find out why  </p>
       </div>
       <h3>Domenic Ceccoli</h3>
       <div class="bio">
         <img src="images\IMG_1976.jpg" alt="Dom Trainer" />
-        <p>Domenic has been a lifetime advocate of core strength, and his ab workout routines have taken him all over the world, earning him the nickname "The Abs Ambassador". Domenic believes that core strength is the baseline of physical fitness, and full body fitness stems from a solid core. Are you looking to get ripped, 6 pack abs for Hot Boy (or Girl) Summer? If so, Dom just might be the perfect trainer for you! Come see why numerous Hollywood actors and actresses have come to Dom before in preparation for their blockbuster films!</p>
+        <p>Domenic has been a lifetime advocate of core strength, and his ab workout routines have taken him all over the world, earning him the nickname <span style="color: #5e62d1; font-weight: bold">"The Abs Ambassador"</span>. Domenic believes that core strength is the baseline of physical fitness, and full body fitness stems from a solid core. Are you looking to get ripped, 6 pack abs for Hot Boy (or Girl) Summer? If so, Dom just might be the perfect trainer for you! Come see why numerous Hollywood actors and actresses have come to Dom before in preparation for their blockbuster films!</p>
       </div>
     </div>
   </body>
