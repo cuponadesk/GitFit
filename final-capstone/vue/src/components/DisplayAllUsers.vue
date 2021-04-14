f07b92077927399e662f5a820e88a5e4f1ae7ba9
Jaclyn Spangler <jaclynkaressa@gmail.com>
2021-04-14 09:34:08 -0400

    adding route to users list

---
+++ final-capstone/vue/src/components/DisplayAllUsers.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>helloooooooo</div>
+</template>
+
+<script>
+//import workoutService from "@/services/WorkoutService.js";
+export default {
+    name: "display-all-users",
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-capstone/vue/src/views/AllUsers.vue
@@ -0,0 +1,16 @@
+<template>
+  <display-all-users />
+</template>
+
+<script>
+import DisplayAllUsers from "@/components/DisplayAllUsers.vue";
+export default {
+    components: {
+    DisplayAllUsers
+    }
+}
+</script>
+
+<style>
+
+</style>
